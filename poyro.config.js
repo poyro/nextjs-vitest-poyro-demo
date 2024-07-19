6c8d39274bba06ee52a886414f8c2d124e9d4ec8
Haldun Anil <haldunanil@gmail.com>
2024-07-18 23:21:58 -0400

    chore: increase the context size

--- poyro.config.js
+++ poyro.config.js
@@ -1,3 +1,9 @@
 import defineConfig from "@poyro/vitest/config";
 
-export default defineConfig({});
+export default defineConfig({
+  llamaCpp: {
+    contextOptions: {
+      contextSize: 1024,
+    },
+  },
+});
